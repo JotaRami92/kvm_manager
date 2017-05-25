37ab9b18a97ce60beada2accd3f7119279bdf0c9
jotarami92 <jgarciana@alumnos.unex.es>
2017-05-25 11:02:35 +0200

    xml

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from PyQt4 import QtGui
-from mainwindow_ui import Ui_MainWindow
-from crearmaquinas_ui import Ui_Dialog1
+from lxml import etree
 from conexiones_ui import Ui_Dialog
-from yattag import Doc, indent
+from crearmaquinas_ui import Ui_Dialog1
+from mainwindow_ui import Ui_MainWindow
+import uuid as u
 
 
 class PopUpConectar(QtGui.QDialog):
-
-
 
     def __init__(self):
         QtGui.QDialog.__init__(self)
@@ -26,7 +25,6 @@ class PopUpConectar(QtGui.QDialog):
     def ok(self):
         self.accept()
 
-
 class PopUpCrearMaquinas(QtGui.QDialog):
 
     def __init__(self):
@@ -36,7 +34,43 @@ class PopUpCrearMaquinas(QtGui.QDialog):
         self.ui.pushButton.clicked.connect(self.finalizar)
         self.ui.pushButton_2.clicked.connect(self.salir)
 
-
+    def generarxml(self, maquina_dict):
+        domain = etree.Element("domain", type="kvm")
+        name = etree.SubElement(domain, "name").text = "%s" % maquina_dict.get('Name')
+        cpu = etree.SubElement(domain, "cpu")
+        topology = etree.SubElement(cpu, "topology", cores="4", sockets="1", threads="4")
+        uuid = etree.SubElement(domain, "uuid").text = "%s" % u.uuid4()
+        memory = etree.SubElement(domain, "memory", unit="MB").text = "%s" % maquina_dict.get('RAM')
+        currentMemory = etree.SubElement(domain, "currentMemory", unit="MB").text = "%s" % maquina_dict.get('RAM')
+        vcpu = etree.SubElement(domain, "vcpu", placement="static").text = "%s" % maquina_dict.get('vcpus')
+        os = etree.SubElement(domain, "os")
+        type = etree.SubElement(os, "type").text = "%s" % maquina_dict.get('os-type')
+        boot = etree.SubElement(os, "boot", dev="hd")
+        features = etree.SubElement(domain, "features")
+        acpi= etree.SubElement(features, "acpi")
+        apic= etree.SubElement(features, "apic")
+        pae= etree.SubElement(features, "pae")
+        clock = etree.SubElement(domain, "clock", offset="utc")
+        on_poweroff = etree.SubElement(domain, "on_poweroff").text = "destroy"
+        on_reboot = etree.SubElement(domain, "on_reboot").text = "restart"
+        on_crash = etree.SubElement(domain, "on_crash").text = "restart"
+        devices = etree.SubElement(domain, "devices")
+        emulator = etree.SubElement(devices, "emulator").text = "/usr/libexec/qemu-kvm"
+        disk = etree.SubElement(devices, "disk", device="disk", type="file")
+        driver = etree.SubElement(disk, "driver", cache="none", name="qemu", type="raw")
+        source = etree.SubElement(disk, "source", file="%s" % maquina_dict.get('disk path'))
+        target = etree.SubElement(disk, "target", dev="hda")
+        address = etree.SubElement(disk, "address", bus="0", controller="0", target="0", type="drive", unit="0")
+        cdrom = etree.SubElement(devices, "disk", device="cdrom", type="file")
+        source2 = etree.SubElement(cdrom, "source", file="%s" % maquina_dict.get('cdrom'))
+        driver2 = etree.SubElement(cdrom, "driver", name="qemu", type="raw")
+        target2 = etree.SubElement(cdrom, "target", bus="ide", dev="hdc")
+        readonly = etree.SubElement(cdrom, "readonly")
+        address2 = etree.SubElement(cdrom, "address", bus="1", controller="0", target="0", type="drive", unit="0")
+        interface = etree.SubElement(devices, "interface", type="network")
+        source_nw = etree.SubElement(interface, "source", network="%s" % maquina_dict.get('network'))
+        graphics = etree.SubElement(devices, "graphics", port="-1", type="vnc")
+        #pasar a fichero
 
 
     def finalizar(self):
@@ -49,14 +83,18 @@ class PopUpCrearMaquinas(QtGui.QDialog):
             lista2.append(str(child.text()))
         for i in range(len(lista1)):
             maquina_dict[lista2[i]] = lista1[i]
+        for k, v in maquina_dict.items():
+            print k
+            print v
+        self.generarxml(maquina_dict)
+
 
         #pasar al crear
         self.accept()
 
+
     def salir(self):
         self.reject()
-
-
 
 class MainUi(QtGui.QMainWindow):
 
@@ -108,8 +146,6 @@ class MainUi(QtGui.QMainWindow):
 
     def cerrar(self):
         exit()
-
-
 
 if __name__ == '__main__':
     import sys
